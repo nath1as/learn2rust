ab400e487b4ab4564c5de28d93fbdb0dd1cec5f5
nathias <n@th1.as>
2023-07-09 11:19:36 +0200

    Update

---
+++ ownership/target/debug/deps/ownership-1dc516025154274e.d
@@ -0,0 +1,5 @@
+/home/nathias/Code/learn2rust/ownership/target/debug/deps/ownership-1dc516025154274e: src/main.rs
+
+/home/nathias/Code/learn2rust/ownership/target/debug/deps/ownership-1dc516025154274e.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/nathias/Code/learn2rust/ownership/target/debug/ownership: /home/nathias/Code/learn2rust/ownership/src/main.rs
