55b1447a28ea628106461a094dab860991c51caa
nathias <n@th1.as>
2023-10-07 09:32:29 +0200

    Add garbage

---
+++ variables/target/debug/deps/variables-335e755ff438c8e4.d
@@ -0,0 +1,5 @@
+/home/nathias/Code/learn2rust/variables/target/debug/deps/variables-335e755ff438c8e4: src/main.rs
+
+/home/nathias/Code/learn2rust/variables/target/debug/deps/variables-335e755ff438c8e4.d: src/main.rs
+
+src/main.rs:
